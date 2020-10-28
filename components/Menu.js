e5622db433bbe88f366d2477dacadc2f6444b38c
Jennifer Kramer <jenkrame@gmail.com>
2020-10-28 19:57:28 -0400

    Played around with the styling more on the + button.

--- components/Menu.js
+++ components/Menu.js
@@ -76,9 +76,10 @@ let menuItems = [
 
   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu-open' on div.menu (your div with a 'menu' class).
 
   Step 5: Don't forget to return your div.menu.
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+  
 */
